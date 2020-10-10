07719248fb02d00a9f747c8767fda0ccfb67325c
jdcast <john.d.cast@gmail.com>
2020-10-09 23:54:56 -0700

    @wip continue structuring styling and layout, switch from flat-ui/bootstrap to shards react and fontawesome icons

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Nav from './Nav';
+import AppNavbar from './Navbar';
 
 class Header extends React.Component {
   render() {
     return (
         <div className="header">
-          <Nav />
-          <div className="navbar navbar-inverse navbar-embossed navbar-expand-lg">
-            <a className="navbar-brand" href="/">Aquaeye AI</a>
-          </div>
+          {/*<Nav />*/}
+          <AppNavbar />
+					{/*<div className="company-name">Aquaeye.ai</div>*/}
         </div>
     );
   }
